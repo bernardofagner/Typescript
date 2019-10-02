8aeb5e220a2fb1bb3645389c998ba3b567b6f455
Fagner de Oliveira Bernardo <bernardo.fagner@hotmail.com>
2019-10-02 18:27:21 -0300

    tipos implicitos

---
+++ introducao_declaracoes/aula_10_declaracao_implicita.ts
@@ -0,0 +1,13 @@
+//Declaracao explicita (expoe o tipo)
+var nome: string = "Fagner";
+
+var idade: number = 10;
+
+//Declaracao implicita (nao expoe o tipo, mas o infere)
+var sobrenome = "Oliveira";
+
+var tempoDeVida = 30;
+
+//Situacoes de erro (descomente para ver)
+//nome = 1;
+//tempoDeVida = "dezoito anos"
