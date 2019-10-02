1b052bafb9e0787f37424dcac2bc2b6256dcac58
Fagner de Oliveira Bernardo <bernardo.fagner@hotmail.com>
2019-10-02 18:01:19 -0300

    introducao a funcoes

---
+++ introducao_declaracoes/aula_9_functions.js
@@ -0,0 +1,6 @@
+function somar(a, b) {
+    return a + b;
+}
+var resultado = somar(3, 5);
+console.log("Result: " + resultado);
+console.log("Resultado" + somar(3, 6));
---
+++ introducao_declaracoes/aula_9_functions.ts
@@ -0,0 +1,7 @@
+function somar(a:number, b:number){
+    return a + b;
+}
+
+var resultado = somar(3, 5);
+console.log("Result: " + resultado);
+console.log("Result: " + somar(3, 6));
