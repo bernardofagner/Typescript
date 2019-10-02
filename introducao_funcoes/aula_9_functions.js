cc23d1d196addb8da61813dd009fcaf940d1b47d
Fagner de Oliveira Bernardo <bernardo.fagner@hotmail.com>
2019-10-02 18:16:44 -0300

    ...

--- introducao_funcoes/aula_9_functions.js
+++ introducao_funcoes/aula_9_functions.js
@@ -1,11 +1,12 @@
 // Atencao aa sintaxe
-function somar(a:number = 0, b:number = 0) :number{
+function somar(a, b) {
+    if (a === void 0) { a = 0; }
+    if (b === void 0) { b = 0; }
     return a + b;
 }
-
 var resultado = somar(3, 5);
 console.log("Result: " + resultado);
-resultado =  somar(3, 6);
+resultado = somar(3, 6);
 console.log("Result: " + resultado);
 resultado = somar(5);
 console.log("Result: " + resultado);
